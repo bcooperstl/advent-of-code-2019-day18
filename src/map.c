d94bb705747b05dedb67d2d93953fdc7c2fa0e65
bcooperstl <coop999@gmail.com>
2020-04-18 13:59:12 -0500

    work in progress

---
+++ src/map.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "map.h"
+
+int isKey(char ch)
+{
+    return (ch >= MIN_KEY && ch <= MAX_KEY);
+}
+
+int isDoor(char ch)
+{
+    return (ch >= MIN_DOOR && ch <= MAX_DOOR);
+}    
+
+int hasKey(map * map, char door)
+{
+    return (map->keys[door-MIN_DOOR]==KEY_OBTAINED ? 1 : 0);
+}
+
+int isWall(map * map, int col, int row)
+{
+    if (col < 0 || col >= map->max_col || row < 0 || row >= map->max_row)
+    {
+        fprintf(stderr, "Error - attempting to check for wall at (%d,%d) while map goes from (0,0) to (%d,%d)\n", col, row, map->max_col, map->max_row);
+        exit(-1);
+    }
+    char val=map->layout[col][row];
+    if (val==WALL)
+        return 1;
+    if (val==SPACE || val==ME)
+        return 0;
+    if (isKey(val)) // keys are never walls
+        return 0;
+    if (isDoor(val))
+    {
+        // doors are walls when we don't have the keys
+        return hasKey(map, val);
+    }
+    fprintf(stderr, "should not get here - unknown value %c(%d) for isWall\n", val, val);
+    exit(-1);
+}
+
+void calculateKeyDistances(map * map)
+{
+    int steps[MAX_MAP_DIMENSION][MAX_MAP_DIMENSION];
+    for (int col=0; col<map->max_col; col++)
+    {
+        for (int row=0; row<map->max_row; row++)
+        {
+            steps[col][row]=NOT_WORKED;
+        }
+    }
+    
+    point location=map->current_location;
+    steps[location.col][location.row]=0;
+    int level = 0;
+    int workToDo = 1;
+    while(workToDo != 0)
+    {
+        int nextLevel=level+1;
+        workToDo=0;
+        for (int col=0; col<map->max_col; col++)
+        {
+            for (int row=0; row<map->max_row; row++)
+            {
+                if (steps[col][row]==level)
+                {
+                    // up
+                    if (row > 0)
+                    {
+                        if ((!isWall(map, col, row-1)) && (steps[col][row-1]==NOT_WORKED))
+                        {
+                            workToDo=1;
+                            steps[col][row-1]=nextLevel;
+                        }
+                    }
+                    // down
+                    if (row < map->max_row-1)
+                    {
+                        if ((!isWall(map, col, row+1)) && (steps[col][row+1]==NOT_WORKED))
+                        {
+                            workToDo=1;
+                            steps[col][row+1]=nextLevel;
+                        }
+                    }
+                    // left
+                    if (col > 0)
+                    {
+                        if ((!isWall(map, col-1, row)) && (steps[col-1][row]==NOT_WORKED))
+                        {
+                            workToDo=1;
+                            steps[col-1][row]=nextLevel;
+                        }
+                    }
+                    // right
+                    if (col < map->max_col-1)
+                    {
+                        if ((!isWall(map, col+1, row)) && (steps[col+1][row]==NOT_WORKED))
+                        {
+                            workToDo=1;
+                            steps[col][row+1]=nextLevel;
+                        }
+                    }
+                }
+            }
+        }
+        level=nextLevel;
+    }
+    for (int i=0; i<MAX_KEYS; i++)
+    {
+        if (map->keys[i]==KEY_NOT_OBTAINED)
+            map->steps_to_key[i]=steps[map->key_location[i].col][map->key_location[i].row];
+    }
+}
---
+++ src/map.h
@@ -0,0 +1,54 @@
+#ifndef __MAP_H__
+#define __MAP_H__
+
+#define MAX_MAP_DIMENSION 96
+#define MAX_KEYS 26
+
+#define DOES_NOT_EXIST 0
+#define KEY_OBTAINED 1
+#define KEY_NOT_OBTAINED 2
+#define DOOR_EXISTS 3
+
+#define NOT_WORKED -1
+
+#define WALL '#'
+#define SPACE '.'
+#define ME '@'
+#define MIN_KEY 'a'
+#define MAX_KEY 'z'
+#define MIN_DOOR 'A'
+#define MAX_DOOR 'Z'
+
+struct point {
+    int col;
+    int row;
+};
+
+typedef struct point point;
+
+
+struct map
+{
+    char layout[MAX_MAP_DIMENSION][MAX_MAP_DIMENSION]; // layout is column, row
+    int max_row;
+    int max_col;
+    point current_location;
+    int doors[MAX_KEYS]; // 26 character array for A-Z
+    point door_location[MAX_KEYS];
+    int keys[MAX_KEYS]; // 26 character array for a-z
+    point key_location[MAX_KEYS];
+    int steps_to_key[MAX_KEYS];
+    struct map * parent;
+    int steps_from_parent;
+    struct map * child_by_key[MAX_KEYS];
+};
+
+typedef struct map map;
+
+int isKey(char ch);
+int isDoor(char ch);
+int hasKey(map * map, char door);
+int isWall(map * map, int col, int row);
+
+
+#endif
